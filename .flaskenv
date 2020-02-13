491d9773036025a5f9e6f4b1ab68e8e423d48d3b
João Paulo <joaopaulo@MacBook-Pro-de-Joao.local>
2020-02-13 06:42:02 -0300

    Start over - Package model

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=site_jm.py
---
+++ app/models.py
@@ -0,0 +1,16 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return f'<User {self.username}'
+
+class Balance(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    operation = db.Column(db.Float)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
