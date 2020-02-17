76cbc1da43d146d0047889e1a63732a828b7a29c
João Paulo <joaopaulo@MacBook-Pro-de-Joao.local>
2020-02-16 21:46:18 -0300

    Bootstrap and CSS

--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,7 @@ def index():
         op = Balance(ammount=form.ammount.data, operation=int(form.operation.data), description=form.description.data, timestamp=form.timestamp.data , user_id=1)
         db.session.add(op)
         db.session.commit()
-        return redirect(url_for('register'))
+        return redirect(url_for('index'))
 
     return render_template('index.html', title='Home', balance=balance, account_statement=account_statement, form = form)
 
@@ -41,19 +41,19 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, parent=form.is_parent.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         user = User(username=form.username.data, email=form.email.data, parent=form.is_parent.data)
+#         user.set_password(form.password.data)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('Congratulations, you are now a registered user!')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
 
 @app.route('/logout')
 def logout():
