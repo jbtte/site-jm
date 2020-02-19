08a81254c402163bd2829a9683845e8688be40c0
João Paulo <joaopaulo@MacBook-Pro-de-Joao.local>
2020-02-19 10:20:19 -0300

    Error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
-from flask import Flask
-from config import Config
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+from flask import Flask, request, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from config import Config
 
 
 app = Flask(__name__)
@@ -15,3 +18,24 @@ login.login_view = 'login'
 bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
+
+
+if not app.debug and not app.testing:
+
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/site_jm.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Site_Jm startup')
--- app/routes.py
+++ app/routes.py
@@ -9,12 +9,12 @@ from werkzeug.urls import url_parse
 @app.route('/index', methods = ["GET", "POST"])
 @login_required
 def index():
-    balance = Balance.current_balance(1)
-    account_statement = Balance.account_statement(1)
+    balance = Balance.current_balance(2)
+    account_statement = Balance.account_statement(2)
     form = ParentInput()
 
     if form.validate_on_submit():
-        op = Balance(ammount=form.ammount.data, operation=int(form.operation.data), description=form.description.data, timestamp=form.timestamp.data , user_id=1)
+        op = Balance(ammount=form.ammount.data, operation=int(form.operation.data), description=form.description.data, timestamp=form.timestamp.data , user_id=2)
         db.session.add(op)
         db.session.commit()
         return redirect(url_for('index'))
